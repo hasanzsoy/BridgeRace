915e2715bc9ce11c72f9125f22668ae1fb55e45a
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-08-06 10:37:50 +0300

    Unityde Visual Studio Ayarları Bozuldu Onlar Düzeltildi

--- Assets/_BridgeRace/Scripts/Characters/TeamColor.cs
+++ Assets/_BridgeRace/Scripts/Characters/TeamColor.cs
@@ -1,16 +1,7 @@
-using UnityEngine;
-
-public class TeamColor : MonoBehaviour
+public enum TeamColor
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Blue,
+    Red,
+    Green,
+    Yellow
 }
