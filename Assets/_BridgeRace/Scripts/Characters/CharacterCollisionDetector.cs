c9ea62485fe071456d42533b0cb604317375456d
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-08-10 10:30:39 +0300

    Oyuncuya çarpınca tuğla düşme sistemi tamamlandı

--- Assets/_BridgeRace/Scripts/Characters/CharacterCollisionDetector.cs
+++ Assets/_BridgeRace/Scripts/Characters/CharacterCollisionDetector.cs
@@ -2,15 +2,60 @@ using UnityEngine;
 
 public class CharacterCollisionDetector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Impact Settings")]
+    [SerializeField] private float minimumImpactSpeed = 2f;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float horizontalKnockbackForce = 7.5f;
+
+    [SerializeField] private float upwardKnockbackForce = 2.2f;
+
+
+    private void OnCollisionEnter(
+        Collision collision)
     {
-        
+        if (collision.relativeVelocity.magnitude <
+            minimumImpactSpeed)
+        {
+            return;
+        }
+
+
+        if (!collision.collider.TryGetComponent<IKnockbackable>(
+                out IKnockbackable knockbackable))
+        {
+            return;
+        }
+
+
+        Vector3 direction =
+            collision.transform.position -
+            transform.position;
+
+
+        direction.y = 0f;
+
+
+        if (direction.sqrMagnitude < 0.001f)
+        {
+            direction =
+                transform.forward;
+        }
+
+
+        direction.Normalize();
+
+
+        Vector3 knockbackForce =
+            direction *
+            horizontalKnockbackForce;
+
+        knockbackForce +=
+            Vector3.up *
+            upwardKnockbackForce;
+
+
+        knockbackable.TakeKnockback(
+            knockbackForce
+        );
     }
 }
